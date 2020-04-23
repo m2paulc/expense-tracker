c7ca70619e9c68aa553302931875949ac58d2e1f
M C <hyp3rsp8c3@gmail.com>
2020-04-23 18:20:35 -0400

    fall animation on delete button not working as expected

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,11 +1,16 @@
 import React from 'react';
 export default class Transactions extends React.Component {
+  state = { isCollapse: false };
+  handleFallAnimation() {
+    this.setState({ isCollapse: true });
+  };
   handleTest() {
     console.log('You clicked Me!');
   };
   handleShowTransactions() {
     return this.props.transactionDetails.map((transaction, idx) => {
       const divRef = React.createRef();
+      const { isCollapse } = this.state;
       return (
         <div key={idx} ref={divRef} className="transaction-container">
           <div className="edit-icon">
@@ -22,7 +27,10 @@ export default class Transactions extends React.Component {
           </div>
           <div className="delete-icon">
             <button onClick={() => {
-              divRef.current.classList.add('fall');
+              // this.handleFallAnimation();
+              divRef.current.value = idx;
+              console.log(divRef.current.value);
+              if (idx === divRef.current.value) divRef.current.classList.add('fall');
               this.props.handleDeleteTransaction(transaction.id);
             }}>
               <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" /><path d="M0 0h24v24H0z" fill="none" /></svg>
